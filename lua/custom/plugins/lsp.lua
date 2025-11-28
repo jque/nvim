c921d5c9fd7e83a7c51be782867d562be597e72a
Jorge Queiruga <jorge.queiruga@embat.com>
2025-11-28 11:02:07 +0000

    feat: Enhance TypeScript/JavaScript development setup
    This commit introduces several improvements for a better development
    experience in TypeScript and JavaScript projects.
    
    - **LSP:** Automatically enables `eslint_d` and `biome` LSP servers via mason to ensure they are available.
    - **Keymaps:** Adds a `gi` keymap for go to implementation and removes a duplicated keymap for go to definition.
    - **Plugins:** Adds the 'ts-error-translator.nvim' plugin to simplify and clarify TypeScript error messages.

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -3,26 +3,28 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 
 return {
-  "mason-org/mason-lspconfig.nvim",
+  'mason-org/mason-lspconfig.nvim',
   opts = {
     automatic_enable = {
-      "ts_ls",
-      "lua_ls",
-      "vimls",
-      "eslint"
-    }
+      'ts_ls',
+      'lua_ls',
+      'vimls',
+      'eslint', -- lsp
+      'eslint_d', -- format
+      'biome',
+    },
   },
   dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
+    { 'mason-org/mason.nvim', opts = {} },
     { 'j-hui/fidget.nvim', opts = {} }, -- lsp status
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     'folke/snacks.nvim',
   },
   config = function(_, opts)
     require('mason-lspconfig').setup(opts)
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(ev)
         local nmap = function(keys, func, desc)
           if desc then
@@ -35,16 +37,17 @@ return {
         nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         nmap('<leader>do', vim.lsp.buf.code_action, '[C]ode [A]ction')
         -- nmap('<leader>gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-        nmap("<leader>gd", require("snacks").picker.lsp_definitions, "[G]oto [D]efinition")
+        nmap('<leader>gd', require('snacks').picker.lsp_definitions, '[G]oto [D]efinition')
         nmap('<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
         nmap('<leader>gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
         nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
         nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
         nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
+        nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
         nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-        nmap("gd", require("snacks").picker.lsp_definitions, "[G]oto [D]efinition")
+        nmap('gd', require('snacks').picker.lsp_definitions, '[G]oto [D]efinition')
         nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
         nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
@@ -56,5 +59,5 @@ return {
         end, '[W]orkspace [L]ist Folders')
       end,
     })
-  end
+  end,
 }
